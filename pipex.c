e0da199fe6c4f6da88047fb3d5219c7f76d1e78a
labderra <labderra@student.42malaga.com>
2024-06-17 08:30:37 +0200

    cambio parent-child

--- pipex.c
+++ pipex.c
@@ -3,16 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: labderra <labderra@student.42.fr>          +#+  +:+       +#+        */
+/*   By: labderra <labderra@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 18:47:07 by labderra          #+#    #+#             */
-/*   Updated: 2024/06/14 13:29:16 by labderra         ###   ########.fr       */
+/*   Updated: 2024/06/16 22:03:39 by labderra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <errno.h>
+#include <unistd.h>
+
+/* 
+Pipex con bonus: 
+- llamo al proceso 
+- proceso hace pipe y fork
+- si es hijo 
+ */
+execve();
 
 void	error(char *str)
 {
@@ -81,51 +91,3 @@ int	main(int argc, char **argv, char **envp)
 	
 	return (0);
 }
-
-       #include <sys/types.h>
-       #include <sys/wait.h>
-       #include <stdio.h>
-       #include <stdlib.h>
-       #include <unistd.h>
-       #include <string.h>
-
-int	main(int argc, char *argv[])
-       {
-           int pipefd[2];
-           pid_t cpid;
-           char buf;
-
-           if (argc != 2) {
-               fprintf(stderr, "Usage: %s <string>\n", argv[0]);
-               exit(EXIT_FAILURE);
-           }
-
-           if (pipe(pipefd) == -1) {
-               perror("pipe");
-               exit(EXIT_FAILURE);
-           }
-
-           cpid = fork();
-           if (cpid == -1) {
-               perror("fork");
-               exit(EXIT_FAILURE);
-           }
-
-           if (cpid == 0) {    /* Child reads from pipe */
-               close(pipefd[1]);          /* Close unused write end */
-
-               while (read(pipefd[0], &buf, 1) > 0)
-                   write(STDOUT_FILENO, &buf, 1);
-
-               write(STDOUT_FILENO, "\n", 1);
-               close(pipefd[0]);
-               _exit(EXIT_SUCCESS);
-
-           } else {            /* Parent writes argv[1] to pipe */
-               close(pipefd[0]);          /* Close unused read end */
-               write(pipefd[1], argv[1], strlen(argv[1]));
-               close(pipefd[1]);          /* Reader will see EOF */
-               wait(NULL);                /* Wait for child */
-               exit(EXIT_SUCCESS);
-           }
-       }
