4e793100a89cb78243e7b4e947210cd6d5f6b2e4
L'Mecki Abderrahaman Elena <labderra@c3r4s5.42malaga.com>
2024-06-18 19:09:43 +0200

    salvando por reinicio

--- pruebas.c
+++ pruebas.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include "ft_printf/ft_printf.h"
 #include "libft/libft.h"
-
+/* 
 int main (int argc, char **argv, char **envp)
 {
 	int		pipefd[2];
@@ -34,7 +34,7 @@ int main (int argc, char **argv, char **envp)
 	}
 	else 
 	{
-/* 		dup2(STDIN_FILENO, pipefd[0]);*/
+ 		dup2(STDIN_FILENO, pipefd[0]);
 		dup2(pipefd[1], STDOUT_FILENO);
 		str = get_next_line(STDIN_FILENO);
 		ft_printf("%s from parent\n", str);
@@ -43,4 +43,73 @@ int main (int argc, char **argv, char **envp)
 		wait(&wstatus);
 	}
 	return (0);
+}
+ */
+
+void	execute(void)
+{
+	char	*str; 
+
+	str = get_next_line(0);
+	while (str)
+	{
+		printf("%s", str);
+		get_next_line(0);
+		free(str);
+	}
+	free(str);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	int		i = 0;
+	int		n = 5;
+	pid_t	cpid;
+	int		fd[2];
+	int		wstatus;
+	(void) argv;
+	(void) argc;
+	(void) envp;
+
+	if (pipe(fd) != 0)
+		exit(EXIT_FAILURE);
+	if ((cpid = fork()) == -1)
+		exit(EXIT_FAILURE);
+	if (cpid != 0)
+	{
+		dup2(fd[1], 1);
+		close(fd[0]);
+		close(fd[1]);
+		execute();
+		waitpid(cpid, &wstatus, 0);
+	}
+	else
+	{
+		while(i < n)
+		{
+			if (cpid != 0)
+			{
+				dup2(fd[1], 1);
+				dup2(fd[0], 0);
+				close(fd[0]);
+				close(fd[1]);
+				execute();
+				waitpid(cpid, &wstatus, 0);
+			}
+			else
+			{			
+				if (pipe(fd) != 0)
+					exit(EXIT_FAILURE);
+				if ((cpid = fork()) == -1)
+					exit(EXIT_FAILURE);
+			}
+			i++;
+		}
+		dup2(fd[1], 1);
+		dup2(fd[0], 0);
+		close(fd[0]);
+		close(fd[1]);
+		execute();
+	}
+
 }
